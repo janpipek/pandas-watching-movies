152604f2f57fd20b8fc1444ddec1e319157e495b
Jakub Urban <coobas@gmail.com>
2022-09-06 20:37:17 +0000

    run_solution()

--- notebooks/solutions.py
+++ notebooks/solutions.py
@@ -1,21 +1,43 @@
 import inspect
+from typing import Any, Callable
+
+import pandas as pd
 from IPython.display import Code
 
 
 def show_solution(exercise_name: str) -> Code:
+    """This displays the source code of a solution."""
+    func = _get_function(exercise_name)
+    source_code = inspect.getsource(func)
+    return Code(source_code, language="python")
+
+
+def run_solution(exercise_name: str, **kwargs) -> Any:
+    """This runs the solution, using locals from the calling namespace."""
+    func = _get_function(exercise_name)
+    frame = inspect.currentframe()
+    caller_locals = (frame.f_back.f_locals)
+
+    arg_names = inspect.getfullargspec(func)[0]
+    
+    full_kwargs = {}
+    full_kwargs.update({name: caller_locals[name] for name in arg_names})
+    full_kwargs.update(kwargs)
+    return func(**full_kwargs)
+
+        
+def solution_10_oldest(movie_titles: pd.DataFrame) -> pd.DataFrame:
+    """Find the 10 oldest movies that are longer than 2 hours."""
+    return movie_titles[movie_titles["runtimeMinutes"] > 120].nsmallest(10, "startYear")
+
+
+def solution_longest_title(movie_titles: pd.DataFrame) -> pd.Series:
+    """Show the row with the movie having the longest (primary) title."""
+    return movie_titles.loc[movie_titles["primaryTitle"].str.len().idxmax()]
+
+
+def _get_function(exercise_name: str) -> Callable:
     try:
-        func = globals()[f"solution_{exercise_name}"]
+        return globals()[f"solution_{exercise_name}"]
     except KeyError:
         print(f"There is no solution defined for {exercise_name}.")
-    else:
-        source_code = inspect.getsource(func)
-        return Code(source_code, language="python")
-        
-
-
-def solution_longest_title(movies):
-    longest_title_movie = "a"
-
-
-if __name__ == "__main__":
-    show_solution("longest_title")
