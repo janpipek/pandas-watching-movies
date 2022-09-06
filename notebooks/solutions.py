c6422d0de6844173871ecafacc2af857b95dca85
Jakub Urban <coobas@gmail.com>
2022-09-06 06:12:17 +0000

    solutions module

---
+++ notebooks/solutions.py
@@ -0,0 +1,19 @@
+import inspect
+
+
+def show_solution(excercise_name: str) -> None:
+    try:
+        func = globals()[f"solution_{excercise_name}"]
+    except KeyError:
+        print(f"There is no solution defined for {exercise_name}.")
+    else:
+        source_code = inspect.getsource(func)
+        print(source_code)
+
+
+def solution_longest_title(movies):
+    longest_title_movie = "a"
+
+
+if __name__ == "__main__":
+    show_solution("longest_title")
