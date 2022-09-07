fdbc0dd090c15067fb60791911a3ac0b2ced4803
Jakub Urban <coobas@gmail.com>
2022-09-07 19:20:47 +0000

    add data

--- notebooks/solutions.py
+++ notebooks/solutions.py
@@ -5,6 +5,8 @@ import pandas as pd
 from IPython.display import Code
 
 
+### Public functions
+
 def show_solution(exercise_name: str) -> Code:
     """This displays the source code of a solution."""
     func = _get_function(exercise_name)
@@ -25,7 +27,9 @@ def run_solution(exercise_name: str, **kwargs) -> Any:
     full_kwargs.update(kwargs)
     return func(**full_kwargs)
 
-        
+
+### Data manipulation
+
 def solution_10_oldest(movie_titles: pd.DataFrame) -> pd.DataFrame:
     """Find the 10 oldest movies that are longer than 2 hours."""
     return movie_titles[movie_titles["runtimeMinutes"] > 120].nsmallest(10, "startYear")
@@ -34,6 +38,17 @@ def solution_10_oldest(movie_titles: pd.DataFrame) -> pd.DataFrame:
 def solution_longest_title(movie_titles: pd.DataFrame) -> pd.Series:
     """Show the row with the movie having the longest (primary) title."""
     return movie_titles.loc[movie_titles["primaryTitle"].str.len().idxmax()]
+
+
+def solution_pink(movie_titles: pd.DataFrame) -> pd.DataFrame:
+    is_pink = movie_titles["primaryTitle"].str.contains("Pink Panther")
+    return movie_titles[is_pink]
+
+
+def solution_hour_length(movie_titles: pd.DataFrame) -> pd.Series:
+    return movies_titles.assign(
+        runtimeHours=movie_titles["runtimeMinutes"] / 60
+    )
 
 
 def _get_function(exercise_name: str) -> Callable:
